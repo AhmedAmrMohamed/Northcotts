8d13e67c817ed8f168283d5e6e7cfeb8e4897b1c
theunderd0g <ahmedbonumstelio@gmail.com>
2018-03-25 00:42:13 +0200

    Update Northcotts.py
    main added
    logging added
    minor bugs fixed(invalid input related)

--- Northcotts.py
+++ Northcotts.py
@@ -1,3 +1,8 @@
+def logtime():
+    import time
+    f=open('log.txt','a')
+    print(time.ctime(),file=f)
+    f.close()
 board=[[1,8] for i in range(8)]
 def build_board():
     '''
@@ -8,7 +13,7 @@ def build_board():
         out.append([])
         for cell in range(1,9):
             if cell ==row[0]:
-                out[-1].append('W')
+                out[-1].append('w')
             elif cell==row[1]:
                 out[-1].append('B')
             else:
@@ -47,13 +52,13 @@ def play(turn,row,to):
     update the board and return tur if the move is valid.
     Otherwise return false
     '''
-    if(row<0 or row>7):
+    if(row<1 or row>8):
         print('Invalid row number')
         return False
+    row-=1
     orgw=board[row][0]
     orgb=board[row][1]
     turn=turn.lower()
-    row-=1
     if(turn=='w'):
         if valid(to,orgb):
             board[row][0]=to
@@ -68,16 +73,16 @@ def play(turn,row,to):
         return False
         print('Invalid Turn Choice')
     return True
-def checkend():
+def end():
     '''
     return false if there is no more valid moves.
     Otherwise return true.
     '''
     for row in board:
         if row[1]-row[0]>1:
-            return True
+            return False
     else:
-        return False
+        return True
 def terminate(turn):
     '''
     output the result. And terminate the program.
@@ -89,3 +94,33 @@ def terminate(turn):
         turn='Black'
     input('{} Has won\npress any key to exit.\n'.format(turn))
     quit()
+def iturn(x):
+    '''
+    inverse the turn for black to white and vice versa
+    '''
+    if x=='w':
+        return 'b'
+    else:
+        return 'w'
+def getinp(turn):
+    while True:
+        inp = input('{} play:'.format(turn)).split()
+        if len(inp)==2:
+            f=open('log.txt','a')
+            print('{} {}'.format(inp[0],inp[1]),file=f)
+            return int(inp[0]),int(inp[1])
+            print('invalid input\nPlease enter the row number of the piece you want to move and it\'s destination\n')
+
+def main():
+    print('Welcom to northcotts\n')
+    logtime()
+    turn='b'
+    while not end():
+        turn=iturn(turn)
+        dis()
+        row,to=getinp(turn)
+        if not play(turn,row,to):
+            turn=iturn(turn)
+    dip()
+    terminate(turn)
+main()
